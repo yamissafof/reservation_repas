8a8c34e9dde14416cb2e57b1f2c815e9e7e78d97
yamissafof <yamissa.fof@gmail.com>
2024-04-16 12:41:32 +0200

    Ajout formulaire de connexion à la route /connexion : test

--- routes.js
+++ routes.js
@@ -1,8 +1,20 @@
 //---------------//
 
+import { createRequire } from "module";
+const require = createRequire(import.meta.url)
+
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 const path = require('path');
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const hostname = '127.0.0.1'
 const port = 3000
@@ -11,7 +23,7 @@ const port = 3000
 
 app.get('/', (requete, resultat) => {
     resultat.sendFile(path.join(__dirname+'/html/accueil.html'));
-    console.log("Page d'accueil du site sur la route : /")
+    console.log('Page d\'accueil du site sur la route : /')
 })
 
 app.get('/inscription', (requete, resultat) => {
@@ -20,6 +32,22 @@ app.get('/inscription', (requete, resultat) => {
 
 app.get('/connexion', (requete, resultat) => {
     resultat.sendFile(path.join(__dirname+'/html/connexion.html'));
+})
+
+app.post('/connexion', (req,res) => {
+    const identifiant = req.body.pseudo;
+    const motDePasse = req.body.motDePasse;
+    if (identifiant === 'admin' && motDePasse === 'admin' ) {
+        res.send('Bonjour mr.l\'admin !')
+    }
+    else {
+        res.send('Login invalide !')
+    }
+})
+
+app.post('/testPost', (requete, resultat) => {
+    let donneesRecues = requete.body;
+    resultat.send('Données reçues : ' + JSON.stringify(donneesRecues))
 })
 
 //----------------//
